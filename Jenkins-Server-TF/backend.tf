da64baa21f94ff6835ddbd2eebbc85df023153f1
Harshal Galande <112915773+Harshalx07@users.noreply.github.com>
2026-03-28 12:24:44 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
+    bucket       = bucket = "dev-harshal-tf-bucket-123"
     region       = "us-east-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
     encrypt      = true
