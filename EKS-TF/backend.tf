e860aea03685e6105ad3d4f33cfecbffeb23d9a0
Harshal Galande <112915773+Harshalx07@users.noreply.github.com>
2026-03-28 12:26:12 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,16 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = bucket = "dev-harshal-tf-bucket-123"
-    region       = "us-east-1"
-    key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    use_lockfile = true
-    encrypt      = true
-  }
-  required_version = ">=1.14.0"
-  required_providers {
-    aws = {
-      version = ">= 5.49.0"
-      source  = "hashicorp/aws"
-    }
+    bucket  = "dev-harshal-tf-bucket-123"
+    key     = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
