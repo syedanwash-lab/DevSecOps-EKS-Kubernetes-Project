67063df582b08c7f8dce414cbd477bd8c7aa93f3
syedanwash-lab <syed.anwash@gmail.com>
2026-03-31 22:38:46 +0530

    Fix formatting and ensure required providers are set

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -6,14 +6,13 @@ terraform {
     encrypt        = true
     dynamodb_table = "anvash-tf-dynamodb"
   }
-}
 
   required_version = ">=1.5.7"
 
   required_providers {
     aws = {
-      version = ">= 6.23.0"
       source  = "hashicorp/aws"
+      version = ">= 6.23.0"
     }
   }
 }
