28bbba6db2e032876055c0a3ab7a07f178bcae36
syedanwash-lab <syed.anwash@gmail.com>
2026-03-31 19:32:49 +0530

    Fix syntax error in backend.tf bucket definition

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = bucket = "anvash-tf-bucket"
+    bucket       = "anvash-tf-bucket"
     region       = "us-east-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
     encrypt      = true
